7d977b1d0d9d809a06e6afc3f1b803370b7ad0a1
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-03 15:04:59 -0500

    added social icons

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
+import { RiInstagramFill, RiDiscordFill } from 'react-icons/ri';
+import { FaTiktok } from 'react-icons/fa6';
+import { FaEnvelope } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <nav className='flex fixed items-center justify-between w-screen h-[10vh] px-[10vw] py-2 z-50 overflow-hidden'>
+    <nav className='flex fixed items-center justify-between w-screen h-[10vh] px-[6vw] py-2 z-50 overflow-hidden'>
       <a href='#home' id='logo' className='flex items-center h-[100%]'>
         <Image
           src='/logoWhiteLong.svg'
@@ -13,7 +16,7 @@ const NavBar = () => {
           className='h-[80%] w-auto'
         />
       </a>
-      <ul className='flex items-center justify-end gap-12 w-[60vw] h-[10vh] mt-3 text-white font-indivisible'>
+      <ul className='flex items-center justify-end gap-12 w-[70vw] h-[10vh] mt-3 text-white font-indivisible'>
         <a href='#home'>
           <li>HOME</li>
         </a>
@@ -33,6 +36,20 @@ const NavBar = () => {
           <li>BOOK SHOP</li>
         </a>
       </ul>
+      <div className='flex items-center gap-4 justify-center h-[10vh] mt-3 pl-12'>
+        <a href=''>
+          <RiInstagramFill className='text-[1.5em]' />
+        </a>
+        <a href=''>
+          <RiDiscordFill className='text-[1.5em]' />
+        </a>
+        <a href=''>
+          <FaTiktok className='text-[1.5em]' />
+        </a>
+        <a href=''>
+          <FaEnvelope className='text-[1.5em]' />
+        </a>
+      </div>
     </nav>
   );
 };
