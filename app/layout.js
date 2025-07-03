eff82e1fab11a66a21090e1fdd0abdd34564013c
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-03 16:33:05 -0500

    please please

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${lavishly.variable} ${a_day_without.variable} antialiased`}
       >
         {children}
       </body>
