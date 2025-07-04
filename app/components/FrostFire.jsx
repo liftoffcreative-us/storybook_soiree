b5963c0d4f235b873970b8be939589caa5d9eb5c
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-04 16:24:54 -0500

    removed snowfall element

--- app/components/FrostFire.jsx
+++ app/components/FrostFire.jsx
@@ -8,7 +8,7 @@ const FrostFire = () => {
       className='relative flex items-center justify-center w-screen h-screen bg-frostFire'
     >
       <div></div>
-      <Snowfall color='rgba(255, 255, 255, .7)' snowflakeCount={200} />
+      {/* <Snowfall color='rgba(255, 255, 255, .7)' snowflakeCount={200} /> */}
     </div>
   );
 };
