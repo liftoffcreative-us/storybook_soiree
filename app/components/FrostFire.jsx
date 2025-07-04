c9b82a24596af34876a6307a0e7eb433a6ac067d
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-04 16:47:54 -0500

    quick update

--- app/components/FrostFire.jsx
+++ app/components/FrostFire.jsx
@@ -5,10 +5,7 @@ const FrostFire = () => {
     <div
       id='frostfire'
       className='relative flex items-center justify-center w-screen h-screen bg-frostFire'
-    >
-      <div></div>
-      {/* <Snowfall color='rgba(255, 255, 255, .7)' snowflakeCount={200} /> */}
-    </div>
+    ></div>
   );
 };
 
