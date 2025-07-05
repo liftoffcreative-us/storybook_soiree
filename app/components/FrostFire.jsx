fe48e561e4218cd0174caf34c96f5fa3cfd31a0a
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-05 17:36:56 -0500

    update with snowfall

--- app/components/FrostFire.jsx
+++ app/components/FrostFire.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Snowfall from 'react-snowfall';
 
 const FrostFire = () => {
   return (
     <div
       id='frostfire'
       className='relative flex items-center justify-center w-screen h-screen bg-frostFire'
-    ></div>
+    >
+      <div className='w-full h-full relative'>
+        <Snowfall />
+      </div>
+    </div>
   );
 };
 
