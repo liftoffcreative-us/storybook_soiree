e8b5f552cc0584957d0a004d784a522b0a323638
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-08 19:28:14 -0500

    fixed a page load error on book club page

--- app/bookclub/page.js
+++ app/bookclub/page.js
@@ -1,12 +1,31 @@
+'use client';
+
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from 'framer-motion';
+import { useRef } from 'react';
 import React from 'react';
 import NavBarAlt from '../components/NavBarAlt';
 import NavBarMobileAlt from '../components/NavBarMobileAlt';
 
 const bookclub = () => {
+  const mainBody = useRef(null);
+
+  const { scrollYProgress: scrollYProgressTarget2 } = useScroll({
+    target: mainBody,
+    offset: ['start end', 'end start'],
+  });
+  useMotionValueEvent(scrollYProgressTarget2, 'change', (latest) => {
+    console.log(latest);
+  });
+
   return (
     <div className='w-screen h-screen'>
-      <NavBarAlt />
-      <NavBarMobileAlt />
+      <NavBarAlt bgColor={'#a48eae'} />
+      <NavBarMobileAlt scroll={scrollYProgressTarget2} />
     </div>
   );
 };
