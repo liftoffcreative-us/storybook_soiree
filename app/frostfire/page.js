a55d958dcb37e4c078747615da71fca96f868cea
liftoffcreative-us <96254128+liftoffcreative-us@users.noreply.github.com>
2025-07-08 19:36:26 -0500

    fixed same issue with frostfire page

--- app/frostfire/page.js
+++ app/frostfire/page.js
@@ -1,12 +1,32 @@
+'use client';
+
+import {
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from 'framer-motion';
+import { useRef } from 'react';
 import React from 'react';
 import NavBarAlt from '../components/NavBarAlt';
 import NavBarMobileAlt from '../components/NavBarMobileAlt';
 
 const frostfire = () => {
+  const mainBody = useRef(null);
+
+  const { scrollYProgress: scrollYProgressTarget2 } = useScroll({
+    target: mainBody,
+    offset: ['start end', 'end start'],
+  });
+  useMotionValueEvent(scrollYProgressTarget2, 'change', (latest) => {
+    console.log(latest);
+  });
   return (
     <div className='w-screen h-screen'>
-      <NavBarAlt />
-      <NavBarMobileAlt />
+      <div className='w-screen h-screen'>
+        <NavBarAlt bgColor={'#a48eae'} />
+        <NavBarMobileAlt scroll={scrollYProgressTarget2} />
+      </div>
     </div>
   );
 };
